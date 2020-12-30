5f18fb76474c5e4c51af7c610ac562261740de82
blapete <pjblank2@gmail.com>
2020-12-30 00:17:04 -0500

    shift return

--- expression.js
+++ expression.js
@@ -15,7 +15,7 @@ const geneExpression = (gene) => {
 	let count = 0;
 	let mRna = transcription(gene);
 	let protein = translation(mRna).split('');
-	 return protein.reduce((x, y) => {
+	return protein.reduce((x, y) => {
 		count++;
 		if (count == 1 || count == protein.length || count == protein.length - 1) {
 			return x + '';
@@ -23,7 +23,6 @@ const geneExpression = (gene) => {
 			return x + y;
 		}
 	}, '');
-
 };
 
 const haemoglobinSubunitBeta = geneExpression(HBB_GENE);
